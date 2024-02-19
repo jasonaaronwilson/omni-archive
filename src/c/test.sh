6f5d90b5a44cab3053dc7193e3edb260c195d3a7
Jason A. Wilson <jasonaaronwilson@gmail.com>
2024-02-19 06:15:02 -0500

    Start keeping track of bugs I won't fix tonight. list only works partly when invoked from test.sh so probably a dumb mistake somewhere.

---
+++ src/c/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x
+rm -rf test-out
+mkdir -p test-out
+
+# Gemini claimed that * on it's own will not match a directory but I
+# think it does though maybe not into the files in such a directory.
+
+### ./oarchive create --ouput-file=test-out/test-archive.oar *
+
+./oarchive create * > test-out/test-archive.oar
+./oarchive list --input-file=test-out/test-archive.oar
+cat test-out/test-archive.oar | ./oarchive list
+
+# ./oarchive create * | ./oarchive list
+# cd test-out
+# ../oarchive list --input-file=test-archive.oar
+
+
+
+
